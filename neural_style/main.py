0d1820a5ae173b7a0336a02d814fa59a39e95be6
houren Antony <129468691+NkAntony777@users.noreply.github.com>
2024-12-16 14:16:30 +0800

    Update main.py
    修改逗号

--- neural_style/main.py
+++ neural_style/main.py
@@ -104,7 +104,7 @@ if clicked:
     try:
         with st.spinner('Stylizing image...'):
             # Load the model and stylize the image
-            model = style.load_model(model_path，weights_only=True)
+            model = style.load_model(model_path,weights_only=True)
            
 
             style.stylize(model, input_image_path, output_image_path)
